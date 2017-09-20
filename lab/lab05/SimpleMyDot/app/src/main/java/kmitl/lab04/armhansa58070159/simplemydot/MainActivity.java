b8b970f45937359adca08541a2d4365cfe576cae
Student <student@LAB308-52.local>
2017-09-20 19:36:01 +0700

    Fix error permission

--- lab/lab05/SimpleMyDot/app/src/main/java/kmitl/lab04/armhansa58070159/simplemydot/MainActivity.java
+++ lab/lab05/SimpleMyDot/app/src/main/java/kmitl/lab04/armhansa58070159/simplemydot/MainActivity.java
@@ -67,6 +67,7 @@ implements Dot.DotChangedListener{
             requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
         }
 
+
     }
 
 
---
+++ lab/lab05/SimpleMyDot/app/src/main/java/kmitl/lab04/armhansa58070159/simplemydot/model/Dot.java
@@ -0,0 +1,56 @@
+package kmitl.lab04.armhansa58070159.simplemydot.model;
+
+import java.util.Random;
+
+public class Dot {
+    public interface DotChangedListener {
+        void onDotChanged(Dot dot);
+    }
+
+    private DotChangedListener dotChangedListener;
+
+    private float centerX;
+    private float centerY;
+    private int radius;
+
+    private int red;
+    private int green;
+    private int blue;
+
+    public Dot(DotChangedListener dotChangedListener, float centerX, float centerY, int radius) {
+        this.dotChangedListener = dotChangedListener;
+        Random random = new Random();
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+        this.red = 100+random.nextInt(155);
+        this.green = 100+random.nextInt(155);
+        this.blue = 100+random.nextInt(155);
+
+        this.dotChangedListener.onDotChanged(this);
+    }
+
+    public float getCenterX() {
+        return centerX;
+    }
+
+    public float getCenterY() {
+        return centerY;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public int getRed() {
+        return red;
+    }
+
+    public int getGreen() {
+        return green;
+    }
+
+    public int getBlue() {
+        return blue;
+    }
+}
