489341f3d1fe38897aac65457fbfe9b5b89e6eb4
armhansa <s8070159@kmitl.ac.th>
2017-11-10 01:15:31 +0700

    Edit Name Lab09

--- lab/lab09/RoomDemo/app/src/main/java/kmitl/armhansa/test/app/roomdemo/MessageDB.java
+++
@@ -1,9 +0,0 @@
-package kmitl.armhansa.test.app.roomdemo;
-
-import android.arch.persistence.room.Database;
-import android.arch.persistence.room.RoomDatabase;
-
-@Database(entities = {MessageInfo.class}, version = 1)
-public abstract class MessageDB extends RoomDatabase {
-    public abstract MessageInfoDAO getMessageInfoDAO();
-}
